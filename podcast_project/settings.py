60ff4728ac42350472fcc08786b878cbf0090c47
Jerry <jerryh@a10networks.com>
2025-05-22 20:36:32 +0800

    BTS-10 User Registration API

--- podcast_project/settings.py
+++ podcast_project/settings.py
@@ -31,14 +31,25 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    ### Built-in Django apps ###
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    ### Business-logic apps ###
     'user_management',
+    'podcast_resources',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
